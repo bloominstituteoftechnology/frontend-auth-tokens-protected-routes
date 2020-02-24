63bcaaf341da08e6b5ea05ef37e243fba8d5ab2b
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-24 16:42:07 +0100

    add a bit of style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Container from './components/Container'
+import './index.css'
 
 ReactDOM.render(
   <Router>
